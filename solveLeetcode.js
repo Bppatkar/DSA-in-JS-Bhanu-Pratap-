e389409cbceb57496c908d1c898ebfac2753c092
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-07-02 17:10:48 +0530

    now starting dsa

--- solveLeetcode.js
+++ solveLeetcode.js
@@ -329,3 +329,114 @@ transpose(matrix); */
 }; */
 
 //! leetcode 1275. Find Winner on a Tic Tac Toe Game
+/* let mp = {
+  A: 'X',
+  B: '0',
+};
+
+function winCheck(grid, player) {
+  //checks row
+  // row 0
+  if (
+    grid[0][0] == mp[player] &&
+    grid[0][1] == mp[player] &&
+    grid[0][2] == mp[player]
+  )
+    return true;
+  // row 1
+  if (
+    grid[1][0] == mp[player] &&
+    grid[1][1] == mp[player] &&
+    grid[1][2] == mp[player]
+  )
+    return true;
+  // row 2
+  if (
+    grid[2][0] == mp[player] &&
+    grid[2][1] == mp[player] &&
+    grid[2][2] == mp[player]
+  )
+    return true;
+
+  //checks col
+  // col 0
+  if (
+    grid[0][0] == mp[player] &&
+    grid[1][0] == mp[player] &&
+    grid[2][0] == mp[player]
+  )
+    return true;
+  // col 1
+  if (
+    grid[0][1] == mp[player] &&
+    grid[1][1] == mp[player] &&
+    grid[2][1] == mp[player]
+  )
+    return true;
+  // col 2
+  if (
+    grid[0][2] == mp[player] &&
+    grid[1][2] == mp[player] &&
+    grid[2][2] == mp[player]
+  )
+    return true;
+
+  //checks diagonal
+  // diagonal 0
+  if (
+    grid[0][0] == mp[player] &&
+    grid[1][1] == mp[player] &&
+    grid[2][2] == mp[player]
+  )
+    return true;
+  // diagonal 1
+  if (
+    grid[2][0] == mp[player] &&
+    grid[1][1] == mp[player] &&
+    grid[0][2] == mp[player]
+  )
+    return true;
+  return false;
+}
+
+var tictactoe = function (moves) {
+  let turn = 0;
+  let grid = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+  ];
+
+  let count = 0;
+  for (let i = 0; i < moves.length; i++) {
+    let currMove = moves[i];
+    let x = currMove[0];
+    let y = currMove[1];
+    let player = turn == 0 ? 'A' : 'B';
+
+    if (turn == 0 && grid[x][y] == ' ') {
+      grid[x][y] = 'X';
+    } else if (turn == 1 && grid[x][y] == ' ') {
+      grid[x][y] = '0';
+    }
+    count++;
+    let doesPlayerWin = winCheck(grid, player);
+    if (doesPlayerWin) {
+      return player;
+    }
+    if (count == 9) {
+      return 'Draw';
+    }
+    turn = (turn + 1) % 2;
+  }
+  return 'Pending';
+}; */
+
+
+
+
+
+
+
+
+
