c9f623cf761195471acb33475afb96db9f30fdf7
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-07-06 22:17:49 +0530

    simply solving by two pointer approach

--- solveLeetcode.js
+++ solveLeetcode.js
@@ -615,3 +615,67 @@ console.log(ans);
  */
 
 //! leetcode 11. Container with Most Water
+// simple two pointer approach
+/* var maxArea = function (height) {
+  let s = 0,
+    ans = 0;
+  let e = height.length - 1;
+  while (s < e) {
+    let width = e - s;
+    ans = Math.max(ans, width * Math.min(height[s], height[e]));
+    if (height[s] <= height[e]) s++;
+    else e--;
+  }
+  return ans;
+};
+let height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+let result = maxArea(height);
+console.log(result); */
+
+//*______________________________________________________________________
+/* 
+“You only need 25 questions to learn all the DSA patterns” — The Coding Interview Bootcamp
+
+Why rush solving 500+ random problems? Save this list of 25 questions that are most frequently asked in tech interviews:
+
+Arrays / Two Pointers / Hashing
+1. 3Sum: https://lnkd.in/gev7gEzS
+2. Container With Most Water: https://lnkd.in/gY2vxRMn
+3. Longest Consecutive Sequence: https://lnkd.in/gMWgaJhS
+
+Strings
+4. Longest Substring Without Repeating Characters: https://lnkd.in/gCPCfm89
+5. Minimum Window Substring: https://lnkd.in/gHn6tqru
+
+Dynamic Programming (DP)
+6. Climbing Stairs: https://lnkd.in/gMAZA-aw
+7. House Robber: https://lnkd.in/g2a7USsS
+8. Longest Palindromic Substring: https://lnkd.in/gKW4rFzN
+9. Coin Change: https://lnkd.in/ggfk-rKw
+10. Partition Equal Subset Sum: https://lnkd.in/guvDMreS
+
+Trees / Binary Trees / BST
+11. Binary Tree Level Order Traversal: https://lnkd.in/gfdAa3qW
+12. Diameter of Binary Tree: https://lnkd.in/gJE6Rf3c
+13. Lowest Common Ancestor of a BST: https://lnkd.in/gaPQwZP7
+
+Backtracking
+14. Subsets: https://lnkd.in/gX9kHr7g
+15. Combination Sum: https://lnkd.in/gPa3pd_R
+16. Word Search: https://lnkd.in/gtAvSg9P
+
+Linked List
+17. Reverse Linked List: https://lnkd.in/g-rU_g2i
+18. Linked List Cycle: https://lnkd.in/g4jxKvVb
+19. Merge Two Sorted Lists: https://lnkd.in/gDwNRASd
+
+Stack
+20. Valid Parentheses: https://lnkd.in/gNX7aNsz
+21. Daily Temperatures: https://lnkd.in/gc-GC4iq
+
+Graphs / BFS / DFS / Union-Find
+22. Number of Islands: https://lnkd.in/g7gxPyqP
+23. Clone Graph: https://lnkd.in/gd5-Xrrf
+24. Course Schedule: https://lnkd.in/gMtQhFeM
+25. Redundant Connection: https://lnkd.in/gNYT4EFR
+*/
