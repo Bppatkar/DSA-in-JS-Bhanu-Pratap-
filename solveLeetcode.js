65ec2d9ad364c1e2aa4433ffe8afd6b5ac5898b2
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-07-09 11:53:00 +0530

    solved

--- solveLeetcode.js
+++ solveLeetcode.js
@@ -651,7 +651,18 @@ let result = maxDistToClosest(seats);
 console.log(result); */
 
 //! leetcode 26. remove duplicate from sorted array
-
+var removeDuplicates = function (nums) {
+  let j = 1;
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] != nums[i + 1]) {
+      nums[j] = nums[i + 1];
+      j++;
+    }
+  }
+  return j;
+};
+let result = removeDuplicates((nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+console.log(result);
 
 //*______________________________________________________________________
 /* 
