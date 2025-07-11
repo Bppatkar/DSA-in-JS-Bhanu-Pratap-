eead09aa8596fcd6bdfa33c13820605dfeabd4b8
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-07-11 09:42:30 +0530

    toggling each character done

--- pratice.js
+++ pratice.js
@@ -1159,3 +1159,20 @@ for (let i = 0; i < str.length; i++) {
 if (isPalindrom) console.log('It is Palindrom');
 else console.log('It is not Palindrom');
  */
+
+//! toggle each character
+// expected output - "AbCdEFgHiJ" --> "aBcDefGhIj"
+// which is in lowercase convert it into uppercase
+
+let str = 'AbCdEFgHiJ';
+console.log(str);
+let toggle = '';
+for (let i = 0; i < str.length  ; i++) {
+  let ch = str.charAt(i);
+  if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
+    toggle += String.fromCharCode(str.charCodeAt(i) + 32);
+  } else if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 120) {
+    toggle += String.fromCharCode(str.charCodeAt(i) - 32);
+  }
+}
+console.log(toggle);
